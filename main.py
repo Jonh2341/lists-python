f14ec4e78813b99fa04c9046d4b3ec41f17467dc
Jonh2341 <rokakaak@gmail.com>
2026-03-29 14:21:33 +0300

    lists basics

---
+++ main.py
@@ -0,0 +1,8 @@
+# lists basics
+favorite_movies = ["Don Juan", "Jack Sparrow", "Alice in Wonderland"]
+favorite_movies.append("Finding Neverland")
+print(favorite_movies)
+favorite_movies.remove("Jack Sparrow")
+print(favorite_movies)
+print(favorite_movies[-1])
+
